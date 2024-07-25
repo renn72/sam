ceab07abe2c54094321d0833bb587dc9ecd57967
renn72 <simplesam8856@gmail.com>
2024-07-25 10:49:19 +1000

    about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -37,38 +37,81 @@ const About = () => {
         />
       </video>
       <div className='container absolute left-0 right-0 top-0 z-10 px-4 md:px-6'>
-        <div className='grid h-screen content-center gap-8 lg:grid-cols-2 lg:gap-12'>
-          <motion.div
-            whileInView={{ x: 0 }}
-            transition={{
-              type: 'spring',
-              duration: 1.5,
-              bounce: 0.5,
-              damping: 10,
-              stiffness: 100,
-            }}
-            initial={{ x: -90 }}
-          >
-            <h2
-              className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl'
+        <div className='grid h-screen content-center gap-8 lg:grid-cols-1 lg:gap-12'>
+          <motion.div className='grid md:gap-24'>
+            <motion.h2
+              whileInView={{ opacity: 1 }}
+              transition={{
+                type: 'spring',
+                duration: 1.5,
+                bounce: 0.5,
+                damping: 10,
+                stiffness: 100,
+              }}
+              className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-center md:text-5xl'
             >
               About Sam
-            </h2>
-            <p className='mt-4 max-w-[600px] tracking-tighter text-muted-foreground text-base md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed'>
-              With over a decade of experience in the food industry, our food
-              development consultant has a deep understanding of the challenges
-              and opportunities facing food businesses. From optimizing recipes
-              to developing innovative menus, we provide comprehensive
-              consulting services to help our clients succeed.
-            </p>
+            </motion.h2>
+            <motion.p
+              whileInView={{ x: 0 }}
+              transition={{
+                type: 'spring',
+                duration: 1.5,
+                bounce: 0.5,
+                damping: 10,
+                stiffness: 100,
+              }}
+              initial={{ x: -190 }}
+              className='mt-4 max-w-[500px] text-base tracking-tighter text-muted-foreground md:justify-self-start md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed'
+            >
+              I am a food and beverage innovator with a catalogue of food
+              solutions. From a hospitality background I leverage the culinary
+              and people skills that come with it.
+            </motion.p>
+            <motion.p
+              whileInView={{ x: 0 }}
+              transition={{
+                type: 'spring',
+                duration: 1.5,
+                bounce: 0.5,
+                damping: 10,
+                stiffness: 100,
+              }}
+              initial={{ x: 190 }}
+              className='mt-4 max-w-[500px] text-base tracking-tighter text-muted-foreground md:justify-self-end md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed'
+            >
+              I work with primary producers and food processors to create
+              products that consumers value. I favour a natural uncomplicated
+              approach to showcase food’s natural attributes and deliver an
+              honest product.
+            </motion.p>
+            <motion.p
+              whileInView={{ x: 0 }}
+              transition={{
+                type: 'spring',
+                duration: 1.5,
+                bounce: 0.5,
+                damping: 10,
+                stiffness: 100,
+              }}
+              initial={{ x: -190 }}
+              className='mt-4 max-w-[500px] text-base tracking-tighter text-muted-foreground md:justify-self-start md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed'
+            >
+              I develop brands using locally grown produce. Incorporating
+              original packaging and design, I can bring a product from the
+              design table to the market.
+            </motion.p>
           </motion.div>
-          <div className='grid gap-6'>
+          <div className='grid hidden gap-6'>
             <div className='grid gap-1'>
               <motion.h3
                 whileInView={{ opacity: 1 }}
                 transition={{ duration: 2.5 }}
                 initial={{ opacity: 0 }}
-                className='text-xl font-bold'>Menu Development</motion.h3>
+                className='text-xl font-bold'
+              >
+                Menu Development
+              </motion.h3>
               <p className='text-muted-foreground'>
                 Craft unique and profitable menus that delight your customers.
               </p>
@@ -78,7 +121,10 @@ const About = () => {
                 whileInView={{ opacity: 1 }}
                 transition={{ duration: 2.5 }}
                 initial={{ opacity: 0 }}
-                className='text-xl font-bold'>Recipe Optimization</motion.h3>
+                className='text-xl font-bold'
+              >
+                Recipe Optimization
+              </motion.h3>
               <p className='text-muted-foreground'>
                 Improve the taste, cost, and efficiency of your recipes.
               </p>
@@ -88,7 +134,10 @@ const About = () => {
                 whileInView={{ opacity: 1 }}
                 transition={{ duration: 2.5 }}
                 initial={{ opacity: 0 }}
-                className='text-xl font-bold'>Food Business Strategy</motion.h3>
+                className='text-xl font-bold'
+              >
+                Food Business Strategy
+              </motion.h3>
               <p className='text-muted-foreground'>
                 Develop a comprehensive strategy to grow your food business.
               </p>
@@ -98,7 +147,10 @@ const About = () => {
                 whileInView={{ opacity: 1 }}
                 transition={{ duration: 2.5 }}
                 initial={{ opacity: 0 }}
-                className='text-xl font-bold'>Culinary Innovation</motion.h3>
+                className='text-xl font-bold'
+              >
+                Culinary Innovation
+              </motion.h3>
               <p className='text-muted-foreground'>
                 Discover new ingredients, techniques, and trends to stay ahead
                 of the competition.
